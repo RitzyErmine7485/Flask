87beab5191865faf739d28ca5584da1e3a578a5f
Ritzy <63694858+RitzyErmine7485@users.noreply.github.com>
2024-09-20 19:59:56 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -21,15 +21,72 @@ def register():
     password = data.get('password')
 
     if mongo.db.users.find_one({"email": email}):
-        return jsonify({"msg": "Ese usuario ya existe"}), 400
+        return jsonify({"msg": "User already exists"}), 400
     
     hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
 
     result = mongo.db.users.insert_one({"username": username,"email": email,"password": hashed_password, "score": 0})
     if result.acknowledged:
-        return jsonify({"msg": "Usuario Creado Correctamente"}), 201
+        return jsonify({"msg": "Success: User created successfully"}), 201
     else:
-        return jsonify({"msg": "Hubo un error, no se pudieron guardar los datos"}),400
+        return jsonify({"msg": "Error: Couldn't save the data"}),400
+
+@app.route('/data', methods=['POST'])
+@jwt_required()
+def datos():
+    data = request.get_json()
+    username = data.get('username')
+
+    usuario = mongo.db.users.find_one({'username': username}, {'password': 0})
+
+    if usuario:
+        usuario['_id'] = str(usuario['_id'])
+        return jsonify({'msg': 'User found:', 'User': usuario}), 200
+    else:
+        return jsonify({'msg': 'User not found'}), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+@app.route('/update', methods=['POST'])
+@jwt_required()
+def update():
+    data = request.get_json()
+    email = data.get('email')
+    password = data.get('password')
+    new_username = data.get('new_username')
+    new_password = data.get('new_password')
+
+    user = mongo.db.users.find_one({'email': email})
+
+    if user and bcrypt.check_password_hash(user['password'], password):
+        update_fields = {}
+        if new_username:
+            update_fields['username'] = new_username
+        if new_password:
+            update_fields['password'] = bcrypt.generate_password_hash(new_password).decode('utf-8')
+
+        if update_fields:
+            mongo.db.users.update_one(user, {'$set': update_fields})
+            return jsonify({'msg': 'Success: User information updated successfully'}), 200
+        else:
+            return jsonify({'msg': 'Error: No update fields provided'}), 400
+    else:
+        return jsonify({'msg': 'Error: Incorrect credentials'}), 401
+
+@app.route('/delete', methods=['POST'])
+@jwt_required()
+def delete():
+    data = request.get_json()
+    email = data.get('email')
+    password = data.get('password')
+
+    user = mongo.db.users.find_one_and_delete({'email': email})
+
+    if user and bcrypt.check_password_hash(user['password'], password):
+        return jsonify('Success: User deleted successfully'), 200
+    else: 
+        return jsonify({'msg': 'Error: Incorret credentials'}), 401
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -43,21 +100,5 @@ def login():
         access_token = create_access_token(identity=str(user['_id']))
         return jsonify(access_token=access_token), 200
     else: 
-        return jsonify({'msg': 'Credenciales incorrectas'}), 401
+        return jsonify({'msg': 'Error: Incorrect credentials'}), 401
 
-@app.route('/data', methods=['POST'])
-@jwt_required()
-def datos():
-    data = request.get_json()
-    username = data.get('username')
-
-    usuario = mongo.db.users.find_one({'username': username}, {'password': 0})
-
-    if usuario:
-        usuario['_id'] = str(usuario['_id'])
-        return jsonify({'msg': 'Usuario encontrado', 'Usuario': usuario}), 200
-    else:
-        return jsonify({'msg': 'Usuario no encontrado'}), 404
-
-if __name__ == '__main__':
-    app.run(debug=True)
